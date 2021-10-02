43acfae147d4c3ba4188e705304a2aa05e231407
henrylopez8 <henrylopez1998.hfl@gmail.com>
2021-10-02 12:34:07 -0600

    version 2

--- paises/App.js
+++ paises/App.js
@@ -1,56 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState,useEffect } from 'react';
-import { StyleSheet, Text, View, Alert} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Alert } from 'react-native';
 import Formulario from './src/components/Formulario';
-import Pais from './src/components/pais'
+import Pais from './src/components/pais';
+
 export default function App() {
   const [busqueda, guardarbusqueda] = useState({
-  pais: '',
+    pais: '',
   });
   const [consultar, guardarconsultar] = useState(false);
   const [resultado, guardarresultado] = useState({});
+  
   useEffect(() => {
   const { pais } = busqueda;
-  const consultarPais = async () => {
-  if (consultar) {
-  const url =
-  `https://servicodados.ibge.gov.br/api/v1/paises/${pais}`;
-  try {
-  const respuesta = await fetch(url);
-  const resultado = await respuesta.json();
-  guardarresultado(resultado);
-  guardarconsultar(false);
-  } catch (error) {
-  mostrarAlerta();
-  }
-  }
-  };
-  consultarPais();
+    const consultarPais = async () => {
+      if (consultar) {
+        const url = `https://servicodados.ibge.gov.br/api/v1/paises/${pais}`;
+        try {
+          const respuesta = await fetch(url);
+          const resultado = await respuesta.json();
+          guardarresultado(resultado);
+          guardarconsultar(false);
+        } catch (error) {
+          mostrarAlerta();
+        }
+      }
+    };
+    consultarPais();
   },[consultar]);
+
   const mostrarAlerta = () => {
-  Alert.alert('Error', 'No hay resultado intenta con otra ciudad o país'),
-  [{ Text: 'Ok' }];
+    Alert.alert('Error', 'No hay resultado intenta con otra ciudad o país'),
+      [{ Text: 'Ok' }];
   };
+
   return (
-  <View style={styles.app}>
-  <View style={styles.contenido}>
-  <Formulario
-  busqueda={busqueda}
-  guardarbusqueda={guardarbusqueda}
-  guardarconsultar={guardarconsultar}
-  />
-  <Pais resultado={resultado} />
-  </View>
-  </View>
+    <View style={styles.app}>
+      <View style={styles.contenido}>
+        
+        
+        <Formulario
+          busqueda={busqueda}
+          guardarbusqueda={guardarbusqueda}
+          guardarconsultar={guardarconsultar}
+        />
+        <Pais resultado={resultado} />
+      </View>
+    </View>
   );
-  }
-  const styles = StyleSheet.create({
+}
+
+const styles = StyleSheet.create({
   app: {
-  flex: 1,
-  backgroundColor: 'rgb(71,149,212)',
-  justifyContent: 'center',
+    flex: 1,
+    backgroundColor: 'rgb(71,149,212)',
+    justifyContent: 'center',
   },
   contenido: {
-  margin: '2.5%',
+    margin: '2.5%',
   },
-  });
+});
